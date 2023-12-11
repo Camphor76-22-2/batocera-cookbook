a40ce8bece04b34fe641cac7beecafeead091a8a
Your mum <you@example.com>
2023-12-11 14:42:04 -0800

    Changed to per disk paths

--- update_roms_folders.sh
+++ update_roms_folders.sh
@@ -3,5 +3,6 @@ for f in $romfolders; do
  echo $f
  mkdir -p $1/$f
  medianame=$(echo "$1" | sed 's/.*\///')
+ mkdir -p /userdata/roms/$f/portal/
  ln -s $1/$f /userdata/roms/$f/portal/$medianame
 done
